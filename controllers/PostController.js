af13ea1fce87946fa32c00cd6ebd6a89ca88041f
ayushvatsa14 <ayushvatsa14@gmail.com>
2025-02-21 17:43:19 +0530

    bug fix

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,43 +1,38 @@
 module.exports = (req, res) => {
-    const { data } = req.body;
-    
-    if(!data || !Array.isArray(data)) {
-        return res.status(400).json({ 
-            is_success: false, 
-            data: "",
-            message: 'Invalid input format'
-        });
+    const { data, filters } = req.body;
+  
+    if (!data || !Array.isArray(data)) {
+      return res.status(400).json({
+        is_success: false,
+        message: 'Invalid input format',
+        data: ''
+      });
     }
   
     const numbers = [];
     const alphabets = [];
   
     data.forEach(value => {
-        if (/^[0-9]+$/.test(value)) {
-            numbers.push(value);
-        } else if (/^[a-zA-Z]$/.test(value)) {
-            alphabets.push(value);
-        }
+      if (/^[0-9]+$/.test(value)) {
+        numbers.push(value);
+      } else if (/^[a-zA-Z]$/.test(value)) {
+        alphabets.push(value);
+      }
     });
-
+    
     let highest_alphabet = '';
-    if(alphabets.length>0) {
-        highest_alphabet=alphabets.reduce((a, b) => (a.toLowerCase() > b.toLowerCase() ? a : b));
+    if (alphabets.length > 0) {
+      highest_alphabet = alphabets.reduce((a, b) => (a.toLowerCase() > b.toLowerCase() ? a : b));
     }
-    
-    const response = {
-        is_success: true,
-        user_id: 'ayush_vatsa_14072004', 
-        email: '22bcs13651@cuchd.in',
-        roll_number: '22bcs13651',
-        numbers,
-        alphabets,
-        highest_alphabet: highest_alphabet ? [highest_alphabet] : []
-    };
-    
+
+    const filteredResponse = {};
+    if (filters.includes('Numbers')) filteredResponse.numbers = numbers;
+    if (filters.includes('Alphabets')) filteredResponse.alphabets = alphabets;
+    if (filters.includes('Highest Alphabet')) filteredResponse.highest_alphabet = highest_alphabet ? [highest_alphabet] : [];
+  
     res.status(200).json({
-        is_success: true,
-        data: response,
-        message: 'Data fetched successfullly'
+      is_success: true,
+      ...filteredResponse,
+      message: 'Filtered data fetched successfully'
     });
-};
+  };  
